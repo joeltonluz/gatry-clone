c287d8268652fd186f4f99cee6818f74c9363ef8
Joelton Lino Luz <joelton@live.com>
2020-03-09 00:42:23 -0300

    modified file routes, add error 404

--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,9 @@ routes.get('/adicionar', promotions.create);
 routes.post('/adicionar', promotions.post);
 routes.get('/termos', promotions.termos);
 
+routes.use((req, res, next) => {
+  res.status(404).render('not-found');
+});
+
+
 module.exports = routes;
